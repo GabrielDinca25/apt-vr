e3a1e3bd6e566e3a0a1eff985b41fe37677a0e0f
GabrielDinca25 <gdincagabriel@gmail.com>
2021-06-06 18:05:15 +0300

    Added potential fix for the resizing of the texture in the EnclosedSpaces scene

--- APT_VR/Assets/Scripts/EnclosedSpaces/EnclosedSpacesController.cs
+++ APT_VR/Assets/Scripts/EnclosedSpaces/EnclosedSpacesController.cs
@@ -86,4 +86,21 @@ public class EnclosedSpacesController : MonoBehaviour
             room.transform.localScale = scale;
         }
     }
+
+//    [SerializeField] private float tileX = 1;
+//    [SerializeField] private float tileY = 1;
+//    Mesh mesh;
+//    private Material mat;
+//    void Start()
+//    {
+//        mat = GetComponent<Renderer>().material;
+//        mesh = GetComponent<MeshFilter>().mesh;
+
+//    }
+
+//    void Update()
+//    {
+//        mat.mainTextureScale = new Vector2((mesh.bounds.size.x *
+//transform.localScale.x) / 100 * tileX, (mesh.bounds.size.y * transform.localScale.y) / 100 * tileY);
+//    }
 }
