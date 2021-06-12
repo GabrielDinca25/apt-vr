eec1b4425974d529e24a0cec66dc0b217794db1c
gogoasealin <gogoasealin@yahoo.com>
2021-06-12 11:42:16 +0300

    Fixed Enclosed Spaces Controller

--- APT_VR/Assets/Scripts/EnclosedSpaces/EnclosedSpacesController.cs
+++ APT_VR/Assets/Scripts/EnclosedSpaces/EnclosedSpacesController.cs
@@ -11,10 +11,17 @@ public class EnclosedSpacesController : MonoBehaviour
     private bool goUp;
     private bool goDown;
 
-    public float resizeSpeed = 0.01f;
+    public float resizeSpeed = 0.1f;
     public float minSize = 0.1f;
     public float maxSize = 1f;
 
+    public Material mat;
+
+    void Start()
+    {
+        room = GameObject.FindGameObjectWithTag("room");
+        mat = room.GetComponent<Renderer>().material;
+    }
 
     void Update()
     {
@@ -85,22 +92,7 @@ public class EnclosedSpacesController : MonoBehaviour
 
             room.transform.localScale = scale;
         }
+
+        mat.mainTextureScale = new Vector2(room.transform.localScale.x, room.transform.localScale.y);
     }
-
-//    [SerializeField] private float tileX = 1;
-//    [SerializeField] private float tileY = 1;
-//    Mesh mesh;
-//    private Material mat;
-//    void Start()
-//    {
-//        mat = GetComponent<Renderer>().material;
-//        mesh = GetComponent<MeshFilter>().mesh;
-
-//    }
-
-//    void Update()
-//    {
-//        mat.mainTextureScale = new Vector2((mesh.bounds.size.x *
-//transform.localScale.x) / 100 * tileX, (mesh.bounds.size.y * transform.localScale.y) / 100 * tileY);
-//    }
 }
